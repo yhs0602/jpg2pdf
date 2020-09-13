1323a3e77d7a67fdcce5334eeeec3fbeb934ea89
KYHSGeekCode <jourhyang123@gmail.com>
2020-09-14 00:03:57 +0900

    :tada: Initial commit

---
+++ jpg2pdf.py
@@ -0,0 +1,19 @@
+import sys
+import os
+# create tex file first
+
+
+with open("tmp.tex", "w", encoding="utf8") as out:
+    out.write("\\documentclass{article}\n")
+    out.write("\\usepackage{float}\n")
+    out.write("\\usepackage{graphicx}\n")
+    out.write("\\usepackage[a4paper, total={6in, 8in}]{geometry}\n")
+    out.write("\\begin{document}\n")
+    for image in sys.argv[1:]:
+        print(image)
+        out.write("\\begin{figure}\n")
+        out.write("\\includegraphics[width=\\linewidth, angle=270]{"+image+"}\n")
+        out.write("\\end{figure}\n")
+        out.write("\\pagebreak\n")
+    out.write("\\end{document}\n")
+os.system("pdflatex tmp.tex")
